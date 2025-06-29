60b4f0dfee23f30c79808c2fd8cb56ba8b106639
Youvasrisoleti <145273518+Youvasrisoleti@users.noreply.github.com>
2025-06-29 13:48:49 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/VIBE-DDD/',
+  base: 'https://github.com/Youvasrisoleti/VIBE-DDD',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
